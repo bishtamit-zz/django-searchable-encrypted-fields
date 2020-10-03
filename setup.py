524f40a90f79c7d9309a10f08c5c46246aaefb09
Guy Willett <guy@chamsoft.co>
2020-10-03 14:30:42 +0000

    Bump to version 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-searchable-encrypted-fields",
-    version="0.1.6",
+    version="0.1.7",
     license="MIT",
     description="Django model fields encrypted using Pycryptodome AES-256 GCM.",
     long_description=open("README.md").read(),
